1574c75ff9ebf4a95735de3b0b080f454bd43893
NandoCruz <nando33_2005@hotmail.com>
2021-04-08 21:23:26 -0300

    Testando o Toast

---
+++ TestandoToast/app/src/main/java/com/example/testandotoast/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.testandotoast
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.d("OnCreate","Mensagem de Teste")
+
+        btnOK.setOnClickListener(View.OnClickListener {
+            Toast.makeText(this,edtNome.text, Toast.LENGTH_LONG).show()
+        })
+    }
+}
